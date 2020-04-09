bbb0afb37bc5dd6ad212bcb16919d311b0f36bd8
Alexander Izrailov <p0028644@ness-tech.co.il>
2020-04-09 04:18:07 +0300

    new branch for handling login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup = null;
 
-  constructor() { }
+  constructor(private readonly _http: HttpClient) {}
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      excelFile: new FormControl(null)
+    });
   }
 
-}
+  /**
+   * @description handle the submit button in form
+   */
+  handleSubmit = () => {
+    this._http
+      .post(
+        "http://localhost:3000/getfile",
+        { data: this.srcresult },
+        {
+          reportProgress: false
+        }
+      )
+      .subscribe(
+        x => {
+          console.log(x);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+  };
+
+  myExcelFile: File;
+  myFiles: FileList;
+  srcresult;
+  handleOnChange = (ev?) => {
+    this.myFiles = ev.target.files;
+    this.myExcelFile = this.myFiles[0];
+
+    console.log(this.myExcelFile);
+
+    let r = new FileReader();
+    r.onload = (ev?) => {
+      this.srcresult = ev.target.result;
+      console.log(this.srcresult);
+    };
+
+    //r.readAsArrayBuffer(this.myExcelFile);
+
+    r.readAsDataURL(this.myExcelFile);
+
+    // r.readAsText(
+    //   this.myExcelFile,
+    //   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    // );
+  };
+} // component class
